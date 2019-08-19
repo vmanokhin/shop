4691d3a185e1d241793a97a71ea48ed80d4f6aba
vitalikmanokhin <vitalikmanokhin@gmail.com>
2019-08-20 01:26:19 +0300

    add mui theme

--- src/Root.js
+++ src/Root.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import store from './redux/store';
 import history from './libs/history';
 import App from './App';
 
+const defaultTheme = createMuiTheme();
 
 function Root() {
     return (
-        <Provider store={store} >
-            <Router history={history}>
-                <App/>
-            </Router>
-        </Provider>
+        <ThemeProvider theme={defaultTheme}>
+            <Provider store={store} >
+                <Router history={history}>
+                    <App/>
+                </Router>
+            </Provider>
+        </ThemeProvider>
     );
 }
 
