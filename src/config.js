4d5a8f2d3b3e55a72d403526d610b81e1d2ec6c0
vmanokhin <vitalikmanokhin@gmail.com>
2019-11-30 16:52:42 +0200

    fix api path & cleanup

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const isDevelopment = process.env.NODE_ENV === 'development';
-export const apiUrl = isDevelopment ? 'http://localhost:3001/api' : 'https://git.heroku.com/shop-mocky/api';
+export const apiUrl = isDevelopment ? 'http://localhost:3001/api' : 'https://shop-mocky.herokuapp.com/api';
 export const appName = 'shop';
