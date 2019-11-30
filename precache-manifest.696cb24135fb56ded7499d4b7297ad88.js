26203f75d5786deb18996e684d86bc5a677259bd
vmanokhin <vitalikmanokhin@gmail.com>
2019-11-30 15:51:00 +0200

    Updates

---
+++ precache-manifest.696cb24135fb56ded7499d4b7297ad88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991cec183a4cf631d72576a87ee161bf",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "98adaeedcb92ac11e5f0",
+    "url": "/shop/static/css/main.cc0c937f.chunk.css"
+  },
+  {
+    "revision": "ebf3b61a9b6eac1a6047",
+    "url": "/shop/static/js/2.16a6d189.chunk.js"
+  },
+  {
+    "revision": "98adaeedcb92ac11e5f0",
+    "url": "/shop/static/js/main.e7d65124.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  }
+]);
