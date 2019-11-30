4e4367d55c3c4bf6091274ee156cccba14a74748
vmanokhin <vitalikmanokhin@gmail.com>
2019-11-30 15:20:18 +0200

    real api

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export const apiUrl = 'http://localhost:3001/api';
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const apiUrl = isDevelopment ? 'http://localhost:3001/api' : 'https://git.heroku.com/shop-mocky/api/';
 export const appName = 'shop';
